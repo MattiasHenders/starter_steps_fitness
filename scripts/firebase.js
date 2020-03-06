ff2a66057f86251ae2371031abbaee3631fadf5f
Vicly Xinyue Cao <viclycao1211@gmail.com>
2020-03-05 16:33:46 -0800

    added main.html, added icons to pages, changed meta info on some pages, added firebase info, linked the pages on some buttons, changed index.html to signup/login page

---
+++ scripts/firebase.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAXfjgWDUyvxARifEK8Lg_lWKinOVRrzPQ",
+    authDomain: "starter-steps-fitness.firebaseapp.com",
+    databaseURL: "https://starter-steps-fitness.firebaseio.com",
+    projectId: "starter-steps-fitness",
+    storageBucket: "starter-steps-fitness.appspot.com",
+    messagingSenderId: "924925976765",
+    appId: "1:924925976765:web:2decbbf78093955c0ad7d4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
