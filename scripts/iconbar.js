f2f5c89020f4d2befda982506c88df9d3a1b3594
Thi Hong Phuc Le <plth1998@gmail.com>
2020-03-15 14:58:25 -0700

    add icon bar to every html pages, add iconbar.js, edit master.css and profile.css

---
+++ scripts/iconbar.js
@@ -0,0 +1,9 @@
+var menus = document.getElementsByTagName('a');
+for (var i = 0; i < menus.length; i++) {
+    menus[i].addEventListener("click", function(){
+        for (var i = 0; i < menus.length; i++) {
+            menus[i].classList.remove("active");
+        }
+        this.classList.toggle("active");
+    });
+}
