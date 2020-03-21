aeb490f4bb1c4cdfaa22747b1750de15538629d5
MattiasHenders <henders.mattias@gmail.com>
2020-03-21 14:12:53 -0700

    Adds weights and reps to workout

--- scripts/workout.js
+++ scripts/workout.js
@@ -1,22 +1,118 @@
-new Promise(function (resolve, reject) {
-    let workoutNames = getWorkout();
-    setTimeout(() => resolve(workoutNames), 2500);
+getUserGoal();
 
-}).then(function (names) {
-    setWorkout(names);
+let goal = localStorage.getItem(0);
+
+new Promise(function (resolve, reject) {
+    let numericals = getNumericals(goal);
+    setTimeout(() => resolve(numericals), 2000);
+
+}).then(function (numericals) {
+    console.log(numericals);
+    setNumericalValues(numericals);
 
 });
 
-function getWorkout() {
-    let name = [];
-    db.collection("exercises").get().then(function (querySnapshot) {
-        querySnapshot.forEach(function (doc) {
-            name.push(doc.data().name);
-        })
+new Promise(function (resolve, reject) {
+    let workout = getWorkout(goal);
+    setTimeout(() => resolve(workout), 2000);
+
+}).then(function (workout) {
+
+    setWorkout(workout);
+
+});
+
+function getUserGoal() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        db.collection("users/").doc(user.uid).onSnapshot(function (snap) {
+            let userGoal = (snap.data().Goal);
+            localStorage.setItem(0, userGoal);
+        });
     })
+}
+/*
+function getReps() {
+    let reps = [];
+    firebase.auth().onAuthStateChanged(function () {
+        db.collection("workouts/gainMuscle/exercises").doc().onSnapshot(function (doc) {
+            reps.push(doc.data()["Rep(#)"]);
+            localStorage.setItem(1, reps);
+        });
+    })
+}
+*/
+function getWorkout(userGoal) {
+    let name = [];
+    if (userGoal == "Gain muscles") {
+        console.log(userGoal);
+        db.collection("workouts/gainMuscle/exercises").get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                console.log(doc.data().name);
+                name.push(doc.data().name);
+            })
+        })
+    } else if (userGoal == "Keep healthy status") {
+        console.log(userGoal);
+        db.collection("workouts/keepHealthy/exercises").get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                console.log(doc.data().name);
+                name.push(doc.data().name);
+            })
+        })
+    } else if (userGoal == "Loss weight") {
+        console.log(userGoal);
+        db.collection("workouts/lossWeight/exercises").get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                console.log(doc.data().name);
+                name.push(doc.data().name);
+            })
+        })
+    } else {
+        console.log("Error in determining goal of user.");
+    }
+
     return name;
 }
 
+function getNumericals(userGoal) {
+    let weight = [];
+    let reps = [];
+    let distance = [];
+    console.log(userGoal);
+    if (userGoal == "Gain muscles") {
+        console.log(userGoal);
+        db.collection("workouts/gainMuscle/exercises").get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                reps.push(doc.data()["Rep(#)"]);
+                weight.push(doc.data()["Weight(lb)"]);
+                distance.push(doc.data()["Distance(km)"]);
+            })
+        })
+    } else if (userGoal == "Keep healthy status") {
+        console.log(userGoal);
+        db.collection("workouts/keepHealthy/exercises").get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                reps.push(doc.data()["Rep(#)"]);
+                weight.push(doc.data()["Weight(lb)"]);
+                distance.push(doc.data()["Distance(km)"]);
+            })
+        })
+    } else if (userGoal == "Loss weight") {
+        console.log(userGoal);
+        db.collection("workouts/lossWeight/exercises").get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                reps.push(doc.data()["Rep(#)"]);
+                weight.push(doc.data()["Weight(lb)"]);
+                distance.push(doc.data()["Distance(km)"]);
+            })
+        })
+    } else {
+        console.log("Error in determining goal of user.");
+    }
+
+    return [reps, weight, distance];
+
+}
 
 function setWorkout(workoutNames) {
     demoID = [];
@@ -32,4 +128,20 @@ function setWorkout(workoutNames) {
         document.getElementById("demo_" + demoID[i]).id = "demo" + (i + 1);
     }
 
+}
+
+// EDIT THIS
+function setNumericalValues(numericalArray) {
+
+    let reps = (numericalArray[0]);
+    let weight = numericalArray[1];
+    let distance = numericalArray[2];
+
+    console.log(reps.length);
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+            document.getElementById("weight" + (i + 1) + "-" + (j + 1)).value = weight[i];
+            document.getElementById("rep" + (i + 1) + "-" + (j + 1)).value = reps[i];
+        }
+    }
 }
